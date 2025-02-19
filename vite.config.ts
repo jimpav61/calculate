dbbf6942988fc05286cd68216d2768fd9f36e25e
jimpav61 <jimmy.pavlatos@gmail.com>
2025-02-19 12:13:59 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/your-repo-name/", // ✅ Change this to your actual GitHub repository name
+  base: "/calculate/", // ✅ Change this to your actual GitHub repository name
   server: {
     host: "::",
     port: 8080,
